da8410e327b8a0c011b00921589a5de20ff1b90a
Work <work@matthews-macbook-pro-2.local>
2016-10-17 21:24:49 -0400

    Initial Commit

---
+++ MedRhythmsBeta/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MedRhythmsBeta
+//
+//  Created by Work on 10/17/16.
+//  Copyright © 2016 Work. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
